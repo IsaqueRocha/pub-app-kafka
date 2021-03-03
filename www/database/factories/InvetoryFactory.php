12250de6892adb23c7da505531f3a0fc48ad4ed5
Isaque Rocha <isaquerocha@gmail.com>
2021-03-03 06:40:09 -0300

    feat: adicionado inventory resource

---
+++ www/database/factories/InvetoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Invetory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InvetoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Invetory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
